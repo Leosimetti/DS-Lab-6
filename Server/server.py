e2b5bd18bd90712853dca21d7efc8479408e1ba6
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2021-08-25 03:23:08 +0700

    AAAAAAAAAAAAAAAAAA

--- Server/server.py
+++ Server/server.py
@@ -7,6 +7,11 @@ import re
 clients = []
 BUFF = 1488 # Unified constant
 
+/**
+ * @todo #234:15m/DEV This is something to do later
+ *  in one of the next releases. I can't figure out
+ *  how to implement it now, that's why the puzzle.
+ */
 
 # Thread to listen one particular client
 class ClientListener(Thread):
